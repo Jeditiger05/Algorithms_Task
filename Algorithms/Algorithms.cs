b6895db39d1a4385a6e3e9e98ce65f7ef0f34ed5
Jeditiger05 <john@jedimedia.it>
2020-04-05 11:40:14 +1000

    First Push of Algorithms Task

---
+++ Algorithms/Algorithms.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Algorithms
+{
+    class Algorithms
+    {
+        public static int BinarySearch(List<int> arr, int lower, int upper, int element, ref int count)
+        {
+            count++;
+            if (upper >= lower)
+            {
+                int mid = lower + (upper - lower) / 2;
+
+                if (arr[mid] == element)
+                {
+                    return mid;
+                }
+
+                if (arr[mid] > element)
+                {
+                    return BinarySearch(arr, lower, mid - 1, element, ref count);
+                }
+
+                return BinarySearch(arr, mid + 1, upper, element, ref count);
+            }
+
+            return -1;
+        }
+
+        public static int LinearSearch(List<int> arr, int num, ref int count)
+        {
+            for (int i = 0; i < arr.Count; i++)
+            {
+                count++;
+                if (arr[i] == num)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public static void ShellSort(List<int> arr, ref int count)
+        {
+            int reduceGap = 2;
+
+            for (int gap = arr.Count / reduceGap; gap > 0; gap /= reduceGap)
+            {
+                count++;
+                for (int i = gap; i < arr.Count; i++)
+                {
+                    count++;
+                    int temp = arr[i];
+
+                    int j;
+                    for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                    {
+                        count++;
+                        arr[j] = arr[j - gap];
+                    }
+
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        public static void InsertionSort(List<int> arr, ref int count)
+        {
+            for (int i = 1; i < arr.Count; ++i)
+            {
+                count++;
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    count++;
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Algorithms
+{
+    class Program
+    {
+        private static Stopwatch stopwatch = new Stopwatch();
+        private static int count = 0;
+
+        static void Main(string[] args)
+        {
+            List<int> arr = new List<int>();
+            int[] searchNum = { 575154, 182339, 17132, 773788, 296934, 991395, 303270, 45231, 580, 629822 };
+            
+            Menu(arr, searchNum);
+        }
+
+        public static void Menu(List<int> arr, int[] searchNum)
+        {
+            
+            int choice = 0;
+            
+
+            while (choice != 8)
+            {
+                Console.WriteLine("Select from the Following Options" +
+                "\n1. Import Elements to Array\n2. Display Original Array" +
+                "\n3. Display Shell Sorted Array\n4. Display Insertion Sorted Array" +
+                "\n5. Linear Search\n6. Binary Search\n7. Time Analysis\n8. Exit");
+
+                Console.Write("Enter Choice:  ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        arr.Clear();
+                        ImportArrayElements(arr);
+                        break;
+                    case 2:
+                        Console.WriteLine("Original Array Elements :");
+                        DisplayArray(arr);
+                        Console.WriteLine("\n");
+
+                        break;
+                    case 3:
+                        count = 0;
+                        Algorithms.ShellSort(arr, ref count);
+                        Console.WriteLine("\nShell Sorted Array Elements :");
+                        DisplayArray(arr);
+                        Console.WriteLine("\nShel Sort Looped: " + count + " times\n");
+                        break;
+                    case 4:
+                        Algorithms.InsertionSort(arr, ref count);
+                        Console.WriteLine("\nInsertion Sorted Array Elements :");
+                        DisplayArray(arr);
+                        Console.WriteLine("\nInsertion Sort Looped: " + count + " times\n");
+                        break;
+                    case 5:
+                        SearchForLinear(arr, searchNum);
+                        break;
+                    case 6:
+                        SearchForBinary(arr, searchNum);
+                        break;
+                    case 7:
+                        count = 0;
+                        SearchAnalysis.AnalysisLinear(arr, searchNum, stopwatch, ref count);
+                        Console.WriteLine("\nLinear Search");
+                        Console.WriteLine($"Looped {count} Times");
+                        Console.WriteLine($"Elapsed Time is {stopwatch.Elapsed}\n");
+                        stopwatch = new Stopwatch();
+                        count = 0;
+                        SearchAnalysis.AnalysisBinary(arr, searchNum, stopwatch, ref count);
+                        Console.WriteLine("Binary Search");
+                        Console.WriteLine($"Looped {count} Times");
+                        Console.WriteLine($"Elapsed Time is {stopwatch.Elapsed}");
+                        Console.WriteLine();
+                        break;
+                    case 8:
+                        break;
+                }
+            }
+        }
+
+        public static void ImportArrayElements(List<int> arr)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(Directory.GetCurrentDirectory() + "\\unsorted_numbers.csv");
+
+                while (!reader.EndOfStream)
+                {
+                    arr.Add(Convert.ToInt32(reader.ReadLine()));
+                }
+                Console.WriteLine("\nFile Imported Successfully\n");
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+
+        }
+
+        public static void SearchForBinary(List<int> arrShell, int[] searchNum)
+        {
+            Console.WriteLine();
+            foreach (int num in searchNum)
+            {
+                int result = Algorithms.BinarySearch(arrShell, 0, arrShell.Count - 1, num, ref count);
+
+                if (result == -1)
+                {
+                    Console.WriteLine("-----------------------------------");
+                    Console.WriteLine("Search Unsuccessful");
+                    Console.WriteLine($"Element {num} not present");
+                }
+                else
+                {
+                    Console.WriteLine("-----------------------------------");
+                    Console.WriteLine("Search successful");
+                    Console.WriteLine($"Element {num} found at index {result}");
+                }
+
+            }
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Binary Search for Elements\n");
+        }
+
+        public static void SearchForLinear(List<int> arr, int[] searchNum)
+        {
+            Console.WriteLine();
+            foreach (int num in searchNum)
+            {
+                if (Algorithms.LinearSearch(arr, num, ref count) > -1)
+                {
+                    Console.WriteLine("-----------------------------------");
+                    Console.WriteLine("Search successful");
+                    Console.WriteLine($"Element {num} found at index {Algorithms.LinearSearch(arr, num, ref count)}");
+                }
+                else
+                {
+                    Console.WriteLine("-----------------------------------");
+                    Console.WriteLine("Search Unsuccessful");
+                    Console.WriteLine($"Element {num} Not Found");
+                }
+            }
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Linear Search for Elements\n");
+        }
+
+        public static void DisplayArray(List<int> arr)
+        {
+            foreach (var element in arr)
+            {
+                Console.Write(element + "\t");
+            }
+        }
+    }
+}
